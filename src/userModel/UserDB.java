0b4a4958df31690e210188cba1ced98f916ae495
camillemartelle <cmll.martelle@gmail.com>
2016-06-05 16:04:11 +0200

    Update UserDB.java

--- src/userModel/UserDB.java
+++ src/userModel/UserDB.java
@@ -64,52 +64,85 @@ public class UserDB {
 	public void setFile(String file) {
 		this.file = file;
 	}
-
+	/**
+	 * 
+	 * @return students
+	 */
 	public ArrayList<Etudiant> getStudents() {
 		return students;
 	}
-
+	/**
+	 *
+	 * @param student 
+	 */
 	public void addStudent(Etudiant student) {
 		students.add(student);
 	}
-
+	/**
+	 * 
+	 * @return teachers
+	 */
 	public ArrayList<Professeur> getTeachers() {
 		return teachers;
 	}
-	
+	/**
+	 * 
+	 * @param professeur
+	 */
 	public void addTeacher(Professeur professeur) {
 		teachers.add(professeur);
 		// TODO Auto-generated method stub
 		
 	}
-
+	/**
+	 * 
+	 * @return admins
+	 */
 	public ArrayList<Admin> getAdmins() {
 		return admins;
 	}
-	
+	/**
+	 * 
+	 * @param admin
+	 */
 	public void addAdmin(Admin admin) {
 		// TODO Auto-generated method stub
 		admins.add(admin);
 		
 	}
-
+	/**
+	 * 
+	 * @param groupe
+	 */
 	public void addGroupe(Groupe groupe) {
 		groupes.add(groupe);
 		
 	}
-	
+	/**
+	 * 
+	 * @return groupes
+	 */
 	public ArrayList<Groupe> getGroupes(){
 		return groupes;
 	}
-	
+	/**
+	 * 
+	 * @param index
+	 */
 	public void removeStudent(int index){
 		students.remove(index);
 	}
-	
+	/**
+	 * 
+	 * @param index
+	 */
 	public void removeAdmin(int index){
 		admins.remove(index);
 	}
-	
+	/**
+	 * 
+	 * @param index
+	 */
 	public void removeTeacher(int index){
 		teachers.remove(index);
 	}
